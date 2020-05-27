88ebe1111c249a3b55fc7df77b6bbb7ad084928f
NazMarch <nazaran237@gmail.com>
2020-05-27 18:02:30 +0300

    Start creating ShopCard

---
+++ Domain/Entities/ShopCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ShopCart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Photo photo, int quantity)
+        {
+            
+        }
+    }
+
+    public class CartLine //One item in shop card
+    {
+        public Photo Photo { get; set; }
+        public int Quantity { get; set; }
+        //public string UserPhoto {get;set}
+    }
+}
